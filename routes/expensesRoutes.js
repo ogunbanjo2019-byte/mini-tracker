fbe933eaaa6ac8646118ef46bae80db0648dc0af
ogunbanjo2019@gmail.com <ogunbanjo2019@gmail.comm>
2026-04-30 10:02:48 +0100

    trackers

---
+++ routes/expensesRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getExpenses,
+  addExpense,
+  deleteExpense,
+} = require('../controllers/expenseController');
+
+router.get('/', getExpenses);
+router.post('/', addExpense);
+router.delete('/:id', deleteExpense);
+
+module.exports = router;
